0922af04e1d265c8cccf36d8b8f60cae4319404f
Duvan <duvan4448@gmail.com>
2020-08-02 16:09:33 -0500

    5-Datepicker

--- angular-api/angular-api/Models/UserViewModel.cs
+++ angular-api/angular-api/Models/UserViewModel.cs
@@ -10,5 +10,7 @@ namespace angular.api.Models
         public string Nombre { get; set; }
         public string Apellido { get; set; }
         public string Email { get; set; }
+
+        public DateTime? Date { get; set; }
     }
 }
